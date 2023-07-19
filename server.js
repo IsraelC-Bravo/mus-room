95df502d7c19fe26120afc3e422a777a1809534e
Israel Castillo <israelocastillo@gmail.com>
2023-07-19 13:32:07 -0600

    first commit

---
+++ server.js
@@ -0,0 +1,65 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const passport = require("passport");
+const session = require("session");
+const MongoStore = require("conect-mongo")(session);
+const methodOverride = require("method-override");
+const flash = require("flash");
+const logger = require("morgan");
+const connectDB = require("./config/database");
+const mainRoutes = require("./routes/main");
+const postRoutes = require("./routes/posts");
+const commentRoutes = require("./routes/comments");
+
+//Use .env file in config folder
+require("dotenv").config({ path: "./config/.env" });
+
+//Passport config
+require("./config/passport")(passport);
+
+//Connect to Database
+connectDB();
+
+//Use EJS for views
+app.set(express.static("public"));
+
+//Static Folder
+app.use(express.static("public"));
+
+//Body Parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//Logging
+app.use(logger("dev"));
+
+//Use forms for put / delete
+app.use(methodOverride("_method"));
+
+//Setup Sessions - stored in MongoDB
+app.use(
+    session({
+        secret: "Keyboard cat",
+        resave: false,
+        saveUninitialized: false,
+        store: new MongoStore({ mongooseConnection: mongoose.connection }),
+    })
+);
+
+//Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+//Use flash messages for errors, info, etc...
+app.use(flash());
+
+//Setup Routes for which the Server is Listening
+app.use("/", mainRoutes);
+app.use("/post", postRoutes);
+app.use("/comment", commentRoutes);
+
+//Server Runinng
+app.listen(process.env.PORT, () => {
+    console.log("Server is running.");
+});
