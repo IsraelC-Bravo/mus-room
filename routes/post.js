33ddb87d14630a5d13a5718762fa1ae413778d1f
Israel Castillo <israelocastillo@gmail.com>
2023-07-21 14:04:38 -0600

    added changes to post router

--- routes/post.js
+++ routes/post.js
@@ -5,16 +5,22 @@ const postController = require("../controllers/posts");
 const { ensureAuth, ensureGuest } = require("../middleware/auth");
 
 //Post Routes
-router.get("/:id", ensureAuth, postsController.getTask); //tasks, not posts
+router.get("/:id", ensureAuth, postsController.getTask);
+//tasks, not posts
 
 router.post("/createTask", upload.single("file"), postController.createTask);
 
-//router.put("/completedTask/:id", postController.completedTask) //mask them completed after work has been sumbitted
-
-router.delete("/deleteTask/:id", postController.deleteTask); //store them after completed? or put in history of completed?
+router.post(
+  "/submitTask/:id",
+  upload.single("file"),
+  postController.submitTask
+);
+//for submitting work
 
 router.put("/likeTask/:id", postController.likeTask); //later, change this to complete task
 
-router.put("/submitTask/:id", upload.single("file"), postController.submitTask); //for submitting work
+//router.put("/completedTask/:id", postController.completedTask) //mark them completed after work has been sumbitted
+
+router.delete("/deleteTask/:id", postController.deleteTask); //store them after completed? or put in history of completed?
 
 module.exports = router;
