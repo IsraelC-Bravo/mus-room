47f5f4e01e38a78ebb384687ca00ad23742564c1
Israel Castillo <israelocastillo@gmail.com>
2023-07-24 14:09:35 -0600

    added User model

---
+++ models/User.js
@@ -0,0 +1,44 @@
+const bcrypt = require("bcrypt");
+const mongoose = require("mongoose");
+
+//userSchema
+const UserSchema = new mongoose.Schema({
+  fisrtName: { type: String, unique: false },
+  lastName: { type: String, unique: false },
+  userName: { type: String, unique: true },
+  email: { type: String, unique: true },
+  password: String,
+  //role: String,
+});
+
+//Password hash midleware
+UserSchema.pre("save", function save(next) {
+  const user = this;
+  if (!user.isModified("password")) {
+    return next();
+  }
+  bcrypt.genSalt(10, (err, salt) => {
+    if (err) {
+      return next(err);
+    }
+    bcrypt.hash(user.password, salt, (err, hash) => {
+      if (err) {
+        return next(err);
+      }
+      user.password = hash;
+      next();
+    });
+  });
+});
+
+//Helper method for validatin user's password
+UserSchema.methods.comparePassword = function comparePassword(
+  candidatePassword,
+  cb
+) {
+  bcrypt.compare(candidatePassword, this.password, (err, isMatch) => {
+    cb(err, isMatch);
+  });
+};
+
+module.exports = mongoose.model("User", UserSchema);
