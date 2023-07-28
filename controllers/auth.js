276a9a5d20743ced70cb2435caca7a0f7bbf4005
Israel Castillo <israelocastillo@gmail.com>
2023-07-28 15:03:33 -0600

    added logut functionality!

--- controllers/auth.js
+++ controllers/auth.js
@@ -53,15 +53,18 @@ exports.postLogin = (req, res, next) => {
 
 //logout
 exports.logout = (req, res) => {
-  req.logout(() => {
-    console.log("User has logged out.");
-  });
-  req.session.destroy((err) => {
+  req.logout((err) => {
     if (err) {
-      console.log("Error : Failed to destroy the session during logout.", err);
+      console.log("Error: Failed to logout.", err);
     }
-    req.user = null;
-    res.redirect("/");
+    console.log("User has logged out.");
+    req.session.destroy((err) => {
+      if (err) {
+        console.log("Error: Failed to destroy the session during logout.", err);
+      }
+      req.user = null;
+      res.redirect("/");
+    });
   });
 };
 
